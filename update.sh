d60d57a8b6b0512e1892e63c6644bf8d298ab13e
FelixFeuerigel <76600556+FelixFeuerigel@users.noreply.github.com>
2022-10-07 15:43:09 +0200

    update db

--- update.sh
+++ update.sh
@@ -50,9 +50,11 @@ for x in "${x86_list[@]}"; do
     if [ $(find . -mindepth 1  -not -name PKGBUILD -not -name *.install | wc -l) -gt 5 ]; then
         updpkgsums PKGBUILD || ( echo "ERROR: FAILED TO UPDATE CHECKSUMS OF PACKAGE: ${x}" && exit 1 )
     fi
+
     makechrootpkg -cur $CHROOT -- -cf || ( echo "ERROR: FAILED TO MAKE PACKAGE: ${x}" && exit 1 )
     # makepkg -cf --sign || echo "FAILED TO MAKE PACKAGE: ${x}"  && exit 1
     # find . -mindepth 1 -maxdepth 1 -type d -print0 | xargs -r0 rm -R
+    
     cd $PKG_DIR
 done
 
@@ -79,7 +81,7 @@ echo "###########################"
 echo "Building the repo database."
 echo "###########################"
 
-rm -f $REPO_NAME*
+rm -f DB_NAME*
 
 echo "############################################"
 echo "Building database for architecture 'x86_64'."
@@ -89,19 +91,19 @@ echo "############################################"
 ## -s: signs the packages
 ## -n: only add new packages not already in database
 ## -R: remove old package files when updating their entry
-repo-add -s -n -R $REPO_NAME.db.tar.gz *.pkg.tar.zst
+repo-add -s -n -R $DB_NAME.db.tar.gz *.pkg.tar.zst
 
 ## Removing the symlinks because GitLab can't handle them.
-rm $REPO_NAME.db
-[[ -d $REPO_NAME.db.sig ]] && rm $REPO_NAME.db.sig
-rm $REPO_NAME.files
-[[ -d $REPO_NAME.files.sig ]] && rm $REPO_NAME.files.sig
+rm $DB_NAME.db
+[[ -d $DB_NAME.db.sig ]] && rm $DB_NAME.db.sig
+rm $DB_NAME.files
+[[ -d $DB_NAME.files.sig ]] && rm $DB_NAME.files.sig
 
 ## Renaming the tar.gz files without the extension.
-mv $REPO_NAME.db.tar.gz $REPO_NAME.db
-[[ -d $REPO_NAME.db.tar.gz.sig ]] && mv $REPO_NAME.db.tar.gz.sig $REPO_NAME-db.sig
-mv $REPO_NAME.files.tar.gz $REPO_NAME.files
-[[ -d $REPO_NAME.files.tar.gz.sig ]] && mv $REPO_NAME.files.tar.gz.sig $REPO_NAME.files.sig
+mv $DB_NAME.db.tar.gz $DB_NAME.db
+[[ -d $DB_NAME.db.tar.gz.sig ]] && mv $DB_NAME.db.tar.gz.sig $DB_NAME-db.sig
+mv $DB_NAME.files.tar.gz $DB_NAME.files
+[[ -d $DB_NAME.files.tar.gz.sig ]] && mv $DB_NAME.files.tar.gz.sig $DB_NAME.files.sig
 
 
 echo "################################"

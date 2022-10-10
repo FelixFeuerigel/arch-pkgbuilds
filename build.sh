3a98d7fdce23dd764b5979b12b3c517ff2cd2197
FelixFeuerigel <76600556+FelixFeuerigel@users.noreply.github.com>
2022-10-10 20:29:00 +0200

    actions update

--- build.sh
+++ build.sh
@@ -17,8 +17,12 @@ PKG_DIR="$(pwd)"
 
 
 ## make chroot evironment if needed
-# mkdir -p "$CHROOT"
-# [[ -d "$CHROOT/root" ]] || mkarchroot -C /etc/pacman.conf "$CHROOT/root" base base-devel
+mkdir -p "$CHROOT"
+chgrp nobody "$CHROOT"
+chmod g+ws "$CHROOT"
+setfacl -m u::rwx,g::rwx "$CHROOT"
+setfacl -d --set u::rwx,g::rwx,o::- "$CHROOT"
+[[ -d "$CHROOT/root" ]] || mkarchroot -C /etc/pacman.conf "$CHROOT/root" base base-devel
 
 ## build packages in "x86_64" directory
 readarray -t x86_list <<< "$(find x86_64/ -type f -name PKGBUILD | awk -F / '{print $2}')"
@@ -34,8 +38,8 @@ for x in "${x86_list[@]}"; do
         updpkgsums PKGBUILD || ( echo "ERROR: FAILED TO UPDATE CHECKSUMS OF PACKAGE: ${x}" && exit 1 )
     fi
 
-    # makechrootpkg -cur $CHROOT -- -cf || ( echo "ERROR: FAILED TO MAKE PACKAGE: ${x}" && exit 1 )
-    sudo -u nobody makepkg -cf || ( echo "ERROR: FAILED TO MAKE PACKAGE: ${x}" && exit 1 )
+    sudo -u nobody makechrootpkg -cur $CHROOT -- -cf || ( echo "ERROR: FAILED TO MAKE PACKAGE: ${x}" && exit 1 )
+    # makepkg -cf || ( echo "ERROR: FAILED TO MAKE PACKAGE: ${x}" && exit 1 )
     # makepkg -cf --sign || echo "FAILED TO MAKE PACKAGE: ${x}"  && exit 1
     # find . -mindepth 1 -maxdepth 1 -type d -print0 | xargs -r0 rm -R
     
